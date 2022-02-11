f76f57254200235b5c629f02848028f30fcac9a9
Rahul19241 <RAHULPALAHWAT@GMAIL.COM>
2022-02-11 13:43:04 +0530

    Initial commit

---
+++ src/components/Body.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+    <>
+    <div className="bodyContainer d-flex justify-content-center align-items-center">
+    <div className="card" style={{width: "50rem", height:"30rem"}}>
+  <div className="card-body">
+    <h1 className="card-title d-flex justify-content-center">Gusto_IIITDMJ</h1>
+    <h2 className="card-subtitle mb-2 text-muted d-flex justify-content-center my-3">Sports Fest</h2>
+    <p className="card-text d-flex justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum quo nobis natus blanditiis ea rem minus eveniet quisquam odio dignissimos adipisci voluptatum, perspiciatis eum repellendus maiores dolore ut porro, dolor labore aut architecto.</p>
+    <p className="card-text d-flex justify-content-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio earum quo nobis natus blanditiis ea rem minus eveniet quisquam odio dignissimos adipisci voluptatum, perspiciatis eum repellendus maiores dolore ut porro, dolor labore aut architecto.</p>
+    <div className="routes d-flex justify-content-between">
+    <button type="button" class="btn btn-primary">Home</button>
+
+    <button type="button" class="btn btn-primary">Book_Ticket</button>
+    </div>
+  </div>
+</div>
+    </div>
+    
+    </>
+  )
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,128 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-dark text-center text-white">
+        <div className="container p-4">
+          <section className="mb-4">
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="/"
+              role="button"
+            >
+              <i class="bi bi-facebook"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="/"
+              role="button"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="/"
+              role="button"
+            >
+              <i className="fab fa-google"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="/"
+              role="button"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="/"
+              role="button"
+            >
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+
+            <a
+              className="btn btn-outline-light btn-floating m-1"
+              href="/"
+              role="button"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </section>
+
+          <section className="">
+            <form action="">
+              <div className="row d-flex justify-content-center">
+                <div className="col-auto">
+                  <p className="pt-2">
+                    <strong>Sign up for our Upcoming Events</strong>
+                  </p>
+                </div>
+
+                <div className="col-md-5 col-12">
+                  <div className="form-outline form-white mb-4">
+                    <input
+                      type="email"
+                      id="form5Example21"
+                      className="form-control" placeholder="Email address"
+                    />
+                    <label className="form-label" HTMLfor="form5Example21">
+                      {/* Email address */}
+                    </label>
+                  </div>
+                </div>
+
+                <div className="col-auto">
+                  <button type="submit" className="btn btn-outline-light mb-4">
+                    Subscribe
+                  </button>
+                </div>
+              </div>
+            </form>
+          </section>
+
+          <section className="mb-4">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, rerum ducimus commodi quos provident aperiam est ab soluta deserunt repudiandae.
+            </p>
+          </section>
+
+          <section className="">
+            <div className="row">
+              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                
+              </div>
+
+              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                
+              </div>
+
+              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                
+              </div>
+
+              <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+                
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div
+          className="text-center p-3"
+          style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}
+        >
+          © 2022 Copyright:
+          
+          Rahul_Hrishi_Jay
+         
+        </div>
+      </footer>
+    </>
+  );
+}
